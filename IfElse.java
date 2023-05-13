ed364692766443f49dcc88f0efc48c72c0e8e4ff
Deepak Jangid <imdjangid123@gmail.com>
2023-05-13 16:43:04 +0530

    Conditional Statement 1 - If Else Statement

---
+++ IfElse.java
@@ -0,0 +1,17 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 7;
+
+        if (x > y)
+        {
+            System.out.println(x);
+            System.out.println("Thank you");
+        }
+        else 
+        {
+            System.out.println(y);
+        }
+    }
+    
+}
