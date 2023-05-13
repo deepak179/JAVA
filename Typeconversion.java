ee2cad1fb60e1e17626837463569b348e640045e
Deepak Jangid <imdjangid123@gmail.com>
2023-05-13 15:06:44 +0530

    Type conversion and casting

--- Typeconversion.java
+++ Typeconversion.java
@@ -1,8 +1,12 @@
 public class Typeconversion {
     public static void main(String[] args) {
-        byte b = 125;
-        int a = b;
+        //byte b = 125;
+        //int a = b;     type conversion
 
-        System.out.println(a);
+        int a = 257;
+        byte b = (byte)a;  // type casting
+
+        System.out.println(a + " Value of integer type ");
+        System.out.println(b + " Value of byte type ");
     }    
 }
