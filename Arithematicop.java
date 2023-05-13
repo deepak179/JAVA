7483444565b282a4118420873e4c0abc4f91fc06
Deepak Jangid <imdjangid123@gmail.com>
2023-05-13 15:44:25 +0530

    Arithematic Operators

---
+++ Arithematicop.java
@@ -0,0 +1,26 @@
+public class Arithematicop 
+{
+    public static void main(String[] args) {
+        //int num1 = 7;
+        //int num2 = 5;
+
+        //int result = num1 + num2;      //ADDITION
+        //int result = num1 - num2;        //SUBTRACT
+        //int result = num1 / num2;      //DIVIDE
+        //int result = num1 % num2;       //REMAINDER
+
+        //num1 = num1 + 2;         //INCREMENT by some value
+        //num1 += 2;
+        //num1 ++;                 //Post - increment
+        //++num1;                 //Pre - increment
+        //num1 -= 2;                //DECREMENT by some value
+        //num1 --;                 //DECREMENT by 1
+
+        int num = 6;
+        int result = num++;       // fetch the value then increment
+        //int result = ++num;       // increment and the fetch the value
+
+        System.out.println(result);
+    }
+    
+}
