b81b4746b8befc52735b67cbdec93821e1417268
Deepak Jangid <imdjangid123@gmail.com>
2023-05-17 19:20:34 +0530

    Array

---
+++ Array.java
@@ -0,0 +1,20 @@
+public class Array {
+    public static void main(String[] args) 
+    {
+        
+        // int[] arr = {1, 2, 3};
+        // arr[1] = 6;
+        // System.out.println(arr[0]);
+
+        int num[] = new int[4];
+        num[0] = 4;
+        num[1] = 2;
+        num[2] = 8;
+        num[3] = 6;
+
+        for (int i = 0; i < num.length; i++) 
+        {
+            System.out.println(num[i]);
+        }
+    }
+}
