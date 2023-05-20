19cb9d382065023cdadc8958f43b6d77e0409568
Deepak Jangid <imdjangid123@gmail.com>
2023-05-20 12:31:57 +0530

    Strings

---
+++ Str.java
@@ -0,0 +1,12 @@
+public class Str {
+    public static void main(String[] args) {
+        String name = "Deepak";
+
+        // String name = new String("Deepak");
+
+        // String name = new String();
+        // name = "Deepak";
+
+        System.out.println("Hello " + name);
+    }
+}
