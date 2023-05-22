777bfe27771762ef376be022a7660ed36517fb46
Deepak Jangid <imdjangid123@gmail.com>
2023-05-22 12:05:44 +0530

    Static Block

---
+++ Staticblock.java
@@ -0,0 +1,40 @@
+class Mobile
+{
+    String brand;
+    int price;
+    static String type;
+    String name;    
+
+    static                           // static block - It will be executed only once when the class is loaded.
+                                     // It is used to initialize static variables
+    {
+        type = "Smartphone";
+        System.out.println("In static block...");
+    }
+
+    public Mobile(String b, int p, String n)                        // constructor
+    {
+        brand = b;
+        price = p;
+        name = n;
+        System.out.println("In constructor");
+    }
+
+    public void display()
+    {
+        System.out.println(brand + " " + name + " : " + type);
+    }
+}
+
+
+public class Staticblock {
+    public static void main(String[] args) {
+        Mobile obj1 = new Mobile("Samsung", 22000, "Galaxy A52");
+        Mobile obj2 = new Mobile("Google", 30000, "Pixel 6a");
+        Mobile obj3 = new Mobile("Apple", 60000, "iphone 13");
+        obj1.display();
+
+
+        // Class.forName("Mobile");    // for loading the class without creating the object
+    }
+}
