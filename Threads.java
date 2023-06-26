e4b28381f20c07a9be2f73e53e90539fad21b7af
Deepak Jangid <imdjangid123@gmail.com>
2023-06-26 10:32:10 +0530

    Thread Priority and Sleep

--- Threads.java
+++ Threads.java
@@ -5,7 +5,12 @@ class A extends Thread
     public void run()       
     {
         for (int i = 0; i < 100; i++)
+        {
             System.out.println("Hi " + i);
+            try{
+                Thread.sleep(10);
+            } catch (InterruptedException e) { e.printStackTrace(); }
+        }
     }
 }
 
@@ -14,7 +19,12 @@ class B extends Thread
     public void run()
     {
         for (int i = 0; i < 100; i++)
+        {
             System.out.println("Hello " + i);
+            try{
+                Thread.sleep(10);
+            } catch (InterruptedException e) { e.printStackTrace(); }
+        }
     }
 }
 
@@ -23,7 +33,14 @@ public class Threads {
         A thrd1 = new A();        // Thread is created
         B thrd2 = new B();
 
+        // System.out.println(thrd1.getPriority());
+        // thrd1.setPriority(Thread.MAX_PRIORITY);
+        // System.out.println(thrd1.getPriority());
+
         thrd1.start();
+        try{
+            Thread.sleep(5);
+        } catch (InterruptedException e) { e.printStackTrace(); }
         thrd2.start();
     }
 }
