bd440b3e3afbbaa0602083e9daa6879c084f4d51
Deepak Jangid <imdjangid123@gmail.com>
2023-06-26 09:46:08 +0530

    Try (Exception Handling) with resources, finally

---
+++ TryWithResources.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TryWithResources {
+    public static void main(String[] args) throws IOException {
+        int num = 0;
+        // BufferedReader br = null;
+
+        // try {
+        //     br = new BufferedReader(new InputStreamReader(System.in));
+        //     num = Integer.parseInt(br.readLine());
+        //     System.out.println(num);
+        // }
+        // finally {
+        //     br.close();
+        // }
+
+        // OR
+
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in)))
+        {
+            num = Integer.parseInt(br.readLine());
+            System.out.println(num);
+        }
+    }
+}
